0d99e085d4549bae3af67e0c78334c68de318368
LordRoad <return_jun@hotmail.com>
2014-06-13 10:28:10 +0800

    move it to inner folder

---
+++ genfilewin/excel_generate/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by excel_generate.rc
+//
+#define IDOK2                           3
+#define IDOK_TESTING                    3
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_EXCEL_GENERATE_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_RADIO_RANDOM                1000
+#define IDC_NUM_ATTR                    1001
+#define IDC_NUM_METRIC                  1002
+#define IDC_NUM_ROWS                    1003
+#define IDC_RADIO_INPUT                 1004
+#define IDC_CUS_NAME                    1005
+#define IDC_COMBO1                      1009
+#define IDC_CHECK_CSV                   1010
+#define IDC_CHECK_XLS                   1011
+#define IDC_CHECK_TXT                   1012
+#define IDC_SLIDER_SPACES_PER           1013
+#define IDC_EDIT_SPACES_NUM_ONECELL     1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
